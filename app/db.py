f0c0c39665330bb293484860b614ffe373cdd534
snehashish <snehashish280999@gmail.com>
2025-01-14 12:22:07 +0530

    added alembic support

--- app/db.py
+++ app/db.py
@@ -9,18 +9,16 @@ sync_engine = create_engine(POSTGRES.POSTGRES_DATABASE_URL)
 Base = declarative_base()
 
 
-class User(Base):
+class TimestampMixin:
+    created = Column(DateTime(timezone=True), nullable=False, default=func.now())
+    updated = Column(DateTime(timezone=True), nullable=False, default=func.now(), onupdate=func.now())
+
+
+class User(TimestampMixin, Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    created = Column(DateTime(timezone=True), nullable=False, default=func.now())
     first_name = Column(String(100))
     last_name = Column(String(100))
     email = Column(String(300), unique=True, nullable=False)
-    phone = Column(String(300), unique=True)
-    photo = Column(String(100))
-    password = Column(String(500),unique=True)
-
-
-Base.metadata.create_all(sync_engine)
 
